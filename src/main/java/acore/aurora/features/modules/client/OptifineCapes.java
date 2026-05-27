952545b656eefd734ffd6ea98fb2a8f901ca95b0
snowmossd-droid <snowmossd@gmail.com>
2026-05-28 03:06:22 +0700

    Delete src directory

--- src/main/java/acore/aurora/features/modules/client/OptifineCapes.java
+++
@@ -1,9 +0,0 @@
-package acore.aurora.features.modules.client;
-
-import acore.aurora.features.modules.Module;
-
-public final class OptifineCapes extends Module {
-    public OptifineCapes() {
-        super("OptifineCapes", Category.CLIENT);
-    }
-}
--- src/main/java/acore/aurora/features/modules/movement/NoWaterCollision.java
+++
@@ -1,9 +0,0 @@
-package acore.aurora.features.modules.movement;
-
-import acore.aurora.features.modules.Module;
-
-public class NoWaterCollision extends Module {
-    public NoWaterCollision() {
-        super("NoWaterCollision", Category.MOVEMENT);
-    }
-}
