423da7e9616eb7854a9d93db795ade201bb11c9c
snowmossd-droid <snowmossd@gmail.com>
2026-05-29 11:05:31 +0700

    Delete src/main directory

--- src/main/java/acore/aurora/features/modules/player/PortalInventory.java
+++
@@ -1,9 +0,0 @@
-package acore.aurora.features.modules.player;
-
-import acore.aurora.features.modules.Module;
-
-public class PortalInventory extends Module {
-    public PortalInventory() {
-        super("PortalInventory", Category.PLAYER);
-    }
-}
