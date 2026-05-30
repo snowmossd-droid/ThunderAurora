ddc39ad7c292dcab74af5387a25b054b3ce027a8
snowmossd-droid <snowmossd@gmail.com>
2026-05-31 05:50:21 +0700

    Update ColorPickerElement.java

--- src/main/java/acore/aurora/gui/clickui/impl/ColorPickerElement.java
+++ src/main/java/acore/aurora/gui/clickui/impl/ColorPickerElement.java
@@ -2,6 +2,7 @@ package acore.aurora.gui.clickui.impl;
 
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.Identifier;
 import acore.aurora.AcoreAurora;
 import acore.aurora.gui.clickui.AbstractElement;
 import acore.aurora.gui.font.FontRenderers;
@@ -22,13 +23,12 @@ public class ColorPickerElement extends AbstractElement {
     private boolean hfocused;
     private boolean sbfocused;
 
-    private float spos, bpos, hpos, apos;
-
     private Color prevColor;
-
-    private boolean firstInit, extended;
+    private boolean extended;
 
     private final Setting colorSetting;
+
+    private static final Identifier COLORS_ICON = Identifier.of("acoreaurora", "textures/gui/elements/colors.png");
 
     public ColorSetting getColorSetting() {
         return (ColorSetting) colorSetting.getValue();
@@ -39,17 +39,17 @@ public class ColorPickerElement extends AbstractElement {
         this.colorSetting = setting;
         prevColor = getColorSetting().getColorObject();
         updatePos();
-        firstInit = true;
     }
 
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-
         MatrixStack matrixStack = context.getMatrices();
 
-        boolean colorHovered = Render2DEngine.isHovered(mouseX, mouseY, x, y + 5f, 90, 7);
+        boolean colorHovered = Render2DEngine.isHovered(mouseX, mouseY, x, y + 3f, width - 5, 11);
 
-        FontRenderers.sf_medium_mini.drawString(matrixStack, setting.getName(), x + 6, y + 8, new Color(-1).getRGB());
+        context.drawTexture(COLORS_ICON, (int)(x + 4), (int)(y + 5), 9, 9, 0, 0, 64, 64, 64, 64);
+
+        FontRenderers.sf_medium_mini.drawString(matrixStack, setting.getName(), x + 16, y + 8, new Color(-1).getRGB());
 
         Render2DEngine.drawBlurredShadow(matrixStack, x + width - 22f, y + 5f, 14, 7, colorHovered ? 6 : 10, getColorSetting().getColorObject());
         if (colorHovered)
@@ -60,96 +60,100 @@ public class ColorPickerElement extends AbstractElement {
         if (!extended)
             return;
 
-        boolean rainbowHovered = Render2DEngine.isHovered(mouseX, mouseY, x + 36f, y + 54f, 24, 7);
-        boolean copyHovered = Render2DEngine.isHovered(mouseX, mouseY, x + 9f, y + 54f, 24, 7);
-        boolean pasteHovered = Render2DEngine.isHovered(mouseX, mouseY, x + 63f, y + 54f, 24, 7);
-        boolean dark = Render2DEngine.isDark(AcoreAurora.copy_color);
-        boolean dark2 = Render2DEngine.isDark(getColorSetting().getColorObject());
-
-        Render2DEngine.drawRect(matrixStack, x + 9f, y + 54f, 24, 7, new Color(0x424242));
-        FontRenderers.sf_medium_mini.drawString(matrixStack, "Сopy", x + 13, y + 56.5f, copyHovered ? new Color(0xA3FFFFFF, true).getRGB() : Color.WHITE.getRGB());
-
-        Render2DEngine.drawRect(matrixStack, x + 36f, y + 54f, 24, 7, getColorSetting().isRainbow() ? getColorSetting().getColorObject() : new Color(0x424242));
-        FontRenderers.sf_medium_mini.drawString(matrixStack, "RB", x + 44f, y + 56.5f, rainbowHovered ? new Color(0xA3FFFFFF, true).getRGB() : (dark2 ? Color.WHITE.getRGB() : Color.BLACK.getRGB()));
-
-        Render2DEngine.drawRect(matrixStack, x + 63f, y + 54f, 24, 7, AcoreAurora.copy_color);
-        FontRenderers.sf_medium_mini.drawString(matrixStack, "Paste", x + 67f, y + 56.5f, pasteHovered ? new Color(0xA3FFFFFF, true).getRGB() : dark ? Color.WHITE.getRGB() : Color.BLACK.getRGB());
-
-        renderPicker(matrixStack, mouseX, mouseY, getColorSetting().getColorObject());
+        renderPicker(context, matrixStack, mouseX, mouseY);
     }
 
     @Override
     public float getHeight() {
-        return extended ? 66 : 15;
+        return extended ? 80 : 15;
     }
 
-    private void renderPicker(MatrixStack matrixStack, int mouseX, int mouseY, Color color) {
-        double cx = x + 6;
-        double cy = y + 16;
-        double cw = width - 38;
-        double ch = height - 30;
-
+    private void renderPicker(DrawContext context, MatrixStack matrixStack, int mouseX, int mouseY) {
         if (prevColor != getColorSetting().getColorObject()) {
             updatePos();
             prevColor = getColorSetting().getColorObject();
         }
 
-        if (firstInit) {
-            spos = (float) ((cx + cw) - (cw - (cw * saturation)));
-            bpos = (float) ((cy + (ch - (ch * brightness))));
-            hpos = (float) ((cy + (ch - 3 + ((ch - 3) * hue))));
-            apos = (float) ((cy + (ch - 3 - ((ch - 3) * (alpha / 255f)))));
-            firstInit = false;
+        float sbX = x + 4;
+        float sbY = y + 18;
+        float sbW = width - 42;
+        float sbH = 42;
+
+        float hueX = x + width - 34;
+        float hueY = sbY;
+        float hueW = 8;
+        float hueH = sbH;
+
+        float alphaX = x + width - 22;
+        float alphaY = sbY;
+        float alphaW = 8;
+        float alphaH = sbH;
+
+        Color satA = Color.getHSBColor(hue, 0f, 1f);
+        Color satB = Color.getHSBColor(hue, 1f, 1f);
+        Render2DEngine.horizontalGradient(matrixStack, sbX, sbY, sbX + sbW, sbY + sbH, satA, satB);
+        Render2DEngine.verticalGradient(matrixStack, sbX, sbY, sbX + sbW, sbY + sbH, new Color(0, 0, 0, 0), new Color(0, 0, 0, 255));
+
+        float dotX = sbX + saturation * sbW;
+        float dotY = sbY + (1f - brightness) * sbH;
+        Render2DEngine.drawRound(matrixStack, dotX - 2.5f, dotY - 2.5f, 5, 5, 2.5f, Color.WHITE);
+        Render2DEngine.drawRound(matrixStack, dotX - 1.5f, dotY - 1.5f, 3, 3, 1.5f, Color.getHSBColor(hue, saturation, brightness));
+
+        for (float i = 0; i < hueH; i += 1f) {
+            float h = i / hueH;
+            Render2DEngine.drawRect(matrixStack, hueX, hueY + i, hueW, 1.5f, Color.getHSBColor(h, 1f, 1f));
         }
+        float hueMarkerY = hueY + hue * hueH;
+        Render2DEngine.drawRect(matrixStack, hueX - 1, hueMarkerY - 1, hueW + 2, 2, Color.WHITE);
 
-        spos = Render2DEngine.scrollAnimate(spos, (float) ((cx + cw) - (cw - (cw * saturation))), .6f);
-        bpos = Render2DEngine.scrollAnimate(bpos, (float) (cy + (ch - (ch * brightness))), .6f);
-        hpos = Render2DEngine.scrollAnimate(hpos, (float) (cy + (ch - 3 + ((ch - 3) * hue))), .6f);
-        apos = Render2DEngine.scrollAnimate(apos, (float) (cy + (ch - 3 - ((ch - 3) * (alpha / 255f)))), .6f);
-
-        Color colorA = Color.getHSBColor(hue, 0.0F, 1.0F), colorB = Color.getHSBColor(hue, 1.0F, 1.0F);
-        Color colorC = new Color(0, 0, 0, 0), colorD = new Color(0, 0, 0);
-
-        Render2DEngine.horizontalGradient(matrixStack, (float) cx + 2, (float) cy, (float) (cx + cw), (float) (cy + ch), colorA, colorB);
-        Render2DEngine.verticalGradient(matrixStack, (float) (cx + 2), (float) cy, (float) (cx + cw), (float) (cy + ch), colorC, colorD);
-
-        for (float i = 1f; i < ch - 2f; i += 1f) {
-            float curHue = (float) (1f / (ch / i));
-            Render2DEngine.drawRect(matrixStack, (float) (cx + cw + 4), (float) (cy + i), 8, 1, Color.getHSBColor(curHue, 1f, 1f));
-        }
-
-        Render2DEngine.drawRect(matrixStack, (float) (cx + cw + 17), (float) (cy + 1f), 8f, (float) (ch - 3), new Color(0xFFFFFFFF));
-
-        Render2DEngine.verticalGradient(matrixStack, (float) (cx + cw + 17), (float) (cy + 0.8f), (float) (cx + cw + 25), (float) (cy + ch - 2), new Color(color.getRed(), color.getGreen(), color.getBlue(), 255), new Color(0, 0, 0, 0));
-
-        Render2DEngine.drawRect(matrixStack, (float) (cx + cw + 3), hpos + 0.5f, 10, 1, Color.WHITE);
-        Render2DEngine.drawRect(matrixStack, (float) (cx + cw + 16), apos + 0.5f, 10, 1, Color.WHITE);
-        Render2DEngine.drawRound(matrixStack, spos - 1.5f, bpos - 1.5f, 3, 3, 1.5f, new Color(-1));
-
-        Color value = Color.getHSBColor(hue, saturation, brightness);
+        Color curRGB = Color.getHSBColor(hue, saturation, brightness);
+        Render2DEngine.verticalGradient(matrixStack, alphaX, alphaY, alphaX + alphaW, alphaY + alphaH,
+                new Color(curRGB.getRed(), curRGB.getGreen(), curRGB.getBlue(), 255),
+                new Color(curRGB.getRed(), curRGB.getGreen(), curRGB.getBlue(), 0));
+        float alphaMarkerY = alphaY + (1f - alpha / 255f) * alphaH;
+        Render2DEngine.drawRect(matrixStack, alphaX - 1, alphaMarkerY - 1, alphaW + 2, 2, Color.WHITE);
 
         if (sbfocused) {
-            saturation = (float) ((MathUtility.clamp((float) (mouseX - cx), 0f, (float) cw)) / cw);
-            brightness = (float) ((ch - MathUtility.clamp((float) (mouseY - cy), 0, (float) ch)) / ch);
-            value = Color.getHSBColor(hue, saturation, brightness);
-            setColor(new Color(value.getRed(), value.getGreen(), value.getBlue(), alpha));
+            saturation = MathUtility.clamp((mouseX - sbX) / sbW, 0f, 1f);
+            brightness = MathUtility.clamp(1f - (mouseY - sbY) / sbH, 0f, 1f);
+            applyColor();
         }
-
         if (hfocused) {
-            hue = (float) -((ch - MathUtility.clamp((float) (mouseY - cy), 0, (float) ch)) / ch);
-            value = Color.getHSBColor(hue, saturation, brightness);
-            setColor(new Color(value.getRed(), value.getGreen(), value.getBlue(), alpha));
+            hue = MathUtility.clamp((mouseY - hueY) / hueH, 0f, 1f);
+            applyColor();
+        }
+        if (afocused) {
+            alpha = (int)(MathUtility.clamp(1f - (mouseY - alphaY) / alphaH, 0f, 1f) * 255);
+            applyColor();
         }
 
-        if (afocused) {
-            alpha = (int) (((ch - MathUtility.clamp((float) (mouseY - cy), 0, (float) ch)) / ch) * 255);
-            setColor(new Color(value.getRed(), value.getGreen(), value.getBlue(), alpha));
-        }
+        float btnY = y + 64;
+        float btnH = 8;
+
+        boolean dark2 = Render2DEngine.isDark(getColorSetting().getColorObject());
+        boolean dark = Render2DEngine.isDark(AcoreAurora.copy_color);
+
+        Render2DEngine.drawRect(matrixStack, x + 4, btnY, 24, btnH, new Color(0x424242));
+        FontRenderers.sf_medium_mini.drawString(matrixStack, "Copy", x + 8, btnY + 2, Color.WHITE.getRGB());
+
+        Render2DEngine.drawRect(matrixStack, x + 32, btnY, 24, btnH, getColorSetting().isRainbow() ? getColorSetting().getColorObject() : new Color(0x424242));
+        FontRenderers.sf_medium_mini.drawString(matrixStack, "RB", x + 40, btnY + 2, dark2 ? Color.WHITE.getRGB() : Color.BLACK.getRGB());
+
+        Render2DEngine.drawRect(matrixStack, x + 60, btnY, 28, btnH, AcoreAurora.copy_color != null ? AcoreAurora.copy_color : new Color(0x424242));
+        FontRenderers.sf_medium_mini.drawString(matrixStack, "Paste", x + 64, btnY + 2, dark ? Color.WHITE.getRGB() : Color.BLACK.getRGB());
+    }
+
+    private void applyColor() {
+        Color c = Color.getHSBColor(hue, saturation, brightness);
+        setColor(new Color(c.getRed(), c.getGreen(), c.getBlue(), alpha));
     }
 
     private void updatePos() {
-        float[] hsb = Color.RGBtoHSB(getColorSetting().getColorObject().getRed(), getColorSetting().getColorObject().getGreen(), getColorSetting().getColorObject().getBlue(), null);
-        hue = -1 + hsb[0];
+        float[] hsb = Color.RGBtoHSB(
+            getColorSetting().getColorObject().getRed(),
+            getColorSetting().getColorObject().getGreen(),
+            getColorSetting().getColorObject().getBlue(), null);
+        hue = hsb[0];
         saturation = hsb[1];
         brightness = hsb[2];
         alpha = getColorSetting().getAlpha();
@@ -162,33 +166,27 @@ public class ColorPickerElement extends AbstractElement {
 
     @Override
     public void mouseClicked(int mouseX, int mouseY, int button) {
-        double cx = x + 4;
-        double cy = y + 17;
-        double cw = width - 34;
-        double ch = height - 30;
-
-        boolean rainbowHovered = Render2DEngine.isHovered(mouseX, mouseY, x + 36f, y + 54f, 24, 7);
-        boolean copyHovered = Render2DEngine.isHovered(mouseX, mouseY, x + 9f, y + 54f, 24, 7);
-        boolean pasteHovered = Render2DEngine.isHovered(mouseX, mouseY, x + 63f, y + 54f, 24, 7);
-        boolean colorHovered = Render2DEngine.isHovered(mouseX, mouseY, x, y, 90, 11);
-
-        if (colorHovered)
+        boolean colorHovered = Render2DEngine.isHovered(mouseX, mouseY, x, y + 3f, width - 5, 11);
+        if (colorHovered) {
             extended = !extended;
-
-        if (!extended)
             return;
+        }
 
-        if (Render2DEngine.isHovered(mouseX, mouseY, cx + cw + 17, cy, 8, ch) && button == 0) afocused = true;
+        if (!extended) return;
 
-        else if (Render2DEngine.isHovered(mouseX, mouseY, cx + cw + 4, cy, 8, ch) && button == 0) hfocused = true;
+        float sbX = x + 4, sbY = y + 18, sbW = width - 42, sbH = 42;
+        float hueX = x + width - 34, alphaX = x + width - 22;
+        float btnY = y + 64, btnH = 8;
 
-        else if (Render2DEngine.isHovered(mouseX, mouseY, cx, cy, cw, ch) && button == 0) sbfocused = true;
-
-        else if (rainbowHovered && button == 0) getColorSetting().setRainbow(!getColorSetting().isRainbow());
-
-        else if (copyHovered) AcoreAurora.copy_color = getColorSetting().getColorObject();
-
-        else if (pasteHovered) getColorSetting().setColor(AcoreAurora.copy_color.getRGB());
+        if (Render2DEngine.isHovered(mouseX, mouseY, sbX, sbY, sbW, sbH) && button == 0) sbfocused = true;
+        else if (Render2DEngine.isHovered(mouseX, mouseY, hueX, sbY, 8, sbH) && button == 0) hfocused = true;
+        else if (Render2DEngine.isHovered(mouseX, mouseY, alphaX, sbY, 8, sbH) && button == 0) afocused = true;
+        else if (Render2DEngine.isHovered(mouseX, mouseY, x + 4, btnY, 24, btnH)) AcoreAurora.copy_color = getColorSetting().getColorObject();
+        else if (Render2DEngine.isHovered(mouseX, mouseY, x + 32, btnY, 24, btnH)) getColorSetting().setRainbow(!getColorSetting().isRainbow());
+        else if (Render2DEngine.isHovered(mouseX, mouseY, x + 60, btnY, 28, btnH) && AcoreAurora.copy_color != null) {
+            getColorSetting().setColor(AcoreAurora.copy_color.getRGB());
+            updatePos();
+        }
 
         super.mouseClicked(mouseX, mouseY, button);
     }
@@ -206,4 +204,5 @@ public class ColorPickerElement extends AbstractElement {
         afocused = false;
         sbfocused = false;
     }
-}
+            }
+                                           
