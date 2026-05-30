80933ebaa9730902121212696f92a2050f66b72f
snowmossd-droid <snowmossd@gmail.com>
2026-05-30 20:00:13 +0700

    Delete src/main directory

--- src/main/java/acore/aurora/events/impl/EventPostSync.java
+++
@@ -1,8 +0,0 @@
-package acore.aurora.events.impl;
-
-import acore.aurora.events.Event;
-
-public class EventPostSync extends Event {
-    public EventPostSync() {
-    }
-}
--- src/main/java/acore/aurora/utility/discord/callbacks/ReadyCallback.java
+++
@@ -1,8 +0,0 @@
-package acore.aurora.utility.discord.callbacks;
-
-import acore.aurora.utility.discord.DiscordUser;
-import com.sun.jna.Callback;
-
-public interface ReadyCallback extends Callback {
-    void apply(final DiscordUser p0);
-}
