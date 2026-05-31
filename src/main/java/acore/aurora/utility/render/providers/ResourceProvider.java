83c90815220ba4470acd9c2b9bef6775069194cf
snowmossd-droid <snowmossd@gmail.com>
2026-06-01 05:01:25 +0700

    Update ResourceProvider.java

--- src/main/java/acore/aurora/utility/render/providers/ResourceProvider.java
+++ src/main/java/acore/aurora/utility/render/providers/ResourceProvider.java
@@ -1,30 +1,8 @@
 package acore.aurora.utility.render.providers;
 
-import net.minecraft.client.render.GameRenderer;
-import net.minecraft.client.render.VertexFormats;
-import net.minecraft.client.render.Shader;
 import net.minecraft.util.Identifier;
 
 public class ResourceProvider {
-    public static Shader getTextureShader() {
-        return GameRenderer.getPositionTexColorProgram();
-    }
-
-    public static Shader getRectangleShader() {
-        return GameRenderer.getPositionColorProgram();
-    }
-
-    public static Shader getBlurShader() {
-        return GameRenderer.getPositionColorProgram();
-    }
-
-    public static Shader getRectangleBorderShader() {
-        return GameRenderer.getPositionColorProgram();
-    }
-
-    public static Shader getGlassShader() {
-        return GameRenderer.getPositionTexColorProgram();
-    }
 
     public static Identifier getShaderIdentifier(String name) {
         return Identifier.of("acoreaurora", "core/shaders/" + name + ".json");
