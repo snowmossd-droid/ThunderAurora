667961d9655746d0e81db87d25356a2acaee2844
snowmossd-droid <snowmossd@gmail.com>
2026-05-31 22:28:04 +0700

    Delete src directory

--- src/main/java/acore/aurora/features/modules/movement/SafeWalk.java
+++
@@ -1,9 +0,0 @@
-package acore.aurora.features.modules.movement;
-
-import acore.aurora.features.modules.Module;
-
-public class SafeWalk extends Module {
-    public SafeWalk() {
-        super("SafeWalk", Category.MOVEMENT);
-    }
-}
