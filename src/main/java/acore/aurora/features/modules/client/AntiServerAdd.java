710f96f3a137cc0b860dab6021422e416cf19009
snowmossd-droid <snowmossd@gmail.com>
2026-06-01 03:56:51 +0700

    Delete src/main directory

--- src/main/java/acore/aurora/features/modules/client/AntiServerAdd.java
+++
@@ -1,9 +0,0 @@
-package acore.aurora.features.modules.client;
-
-import acore.aurora.features.modules.Module;
-
-public class AntiServerAdd extends Module { //эмпти модул
-    public AntiServerAdd() {
-        super("AntiServerAdd", Category.CLIENT);
-    }
-}
--- src/main/java/acore/aurora/features/modules/player/AntiBallPlace.java
+++
@@ -1,8 +0,0 @@
-package acore.aurora.features.modules.player;
-
-import acore.aurora.features.modules.Module;
-public class AntiBallPlace extends Module {
-    public AntiBallPlace() {
-        super("AntiBallPlace", Category.PLAYER);
-    }
-}
