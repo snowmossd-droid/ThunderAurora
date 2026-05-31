891dc0371c744860831de190e8fd9341c3bad282
snowmossd-droid <snowmossd@gmail.com>
2026-06-01 05:47:28 +0700

    Delete src/main directory

--- src/main/java/acore/aurora/features/modules/movement/EntityControl.java
+++
@@ -1,9 +0,0 @@
-package acore.aurora.features.modules.movement;
-
-import acore.aurora.features.modules.Module;
-
-public class EntityControl extends Module {
-    public EntityControl() {
-        super("EntityControl", Category.MOVEMENT);
-    }
-}
