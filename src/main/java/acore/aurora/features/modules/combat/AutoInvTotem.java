d4e413ec9052e829cb68812c5462e00f8674e6a9
snowmossd-droid <snowmossd@gmail.com>
2026-06-01 06:48:33 +0700

    Update AutoInvTotem.java

--- src/main/java/acore/aurora/features/modules/combat/AutoInvTotem.java
+++ src/main/java/acore/aurora/features/modules/combat/AutoInvTotem.java
@@ -8,6 +8,7 @@ import meteordevelopment.meteorclient.settings.BoolSetting;
 import meteordevelopment.meteorclient.settings.IntSetting;
 import meteordevelopment.meteorclient.settings.Setting;
 import meteordevelopment.meteorclient.settings.SettingGroup;
+import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.client.gui.screen.ingame.InventoryScreen;
