162a9bf2359aa34b15a15cc6b478a8093555acb9
snowmossd-droid <snowmossd@gmail.com>
2026-06-01 04:51:13 +0700

    Update ResourceProvider.java

--- src/main/java/acore/aurora/utility/render/providers/ResourceProvider.java
+++ src/main/java/acore/aurora/utility/render/providers/ResourceProvider.java
@@ -1,15 +1,30 @@
 package acore.aurora.utility.render.providers;
 
-import net.minecraft.client.gl.ShaderProgramKeys;
+import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.render.VertexFormats;
+import net.minecraft.client.render.Shader;
 import net.minecraft.util.Identifier;
 
 public class ResourceProvider {
-    public static final ShaderProgramKeys TEXTURE_SHADER_KEY = ShaderProgramKeys.POSITION_TEX_COLOR;
-    public static final ShaderProgramKeys RECTANGLE_SHADER_KEY = ShaderProgramKeys.POSITION_COLOR;
-    public static final ShaderProgramKeys BLUR_SHADER_KEY = ShaderProgramKeys.POSITION_COLOR;
-    public static final ShaderProgramKeys RECTANGLE_BORDER_SHADER_KEY = ShaderProgramKeys.POSITION_COLOR;
-    public static final ShaderProgramKeys GLASS_SHADER_KEY = ShaderProgramKeys.POSITION_TEX_COLOR;
+    public static Shader getTextureShader() {
+        return GameRenderer.getPositionTexColorProgram();
+    }
+
+    public static Shader getRectangleShader() {
+        return GameRenderer.getPositionColorProgram();
+    }
+
+    public static Shader getBlurShader() {
+        return GameRenderer.getPositionColorProgram();
+    }
+
+    public static Shader getRectangleBorderShader() {
+        return GameRenderer.getPositionColorProgram();
+    }
+
+    public static Shader getGlassShader() {
+        return GameRenderer.getPositionTexColorProgram();
+    }
 
     public static Identifier getShaderIdentifier(String name) {
         return Identifier.of("acoreaurora", "core/shaders/" + name + ".json");
