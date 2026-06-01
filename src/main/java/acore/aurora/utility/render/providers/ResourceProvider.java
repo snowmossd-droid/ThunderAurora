1b0561c6d63c00e1ec9d2301d09d30d96d0bdb85
snowmossd-droid <snowmossd@gmail.com>
2026-06-02 03:29:19 +0700

    Update ResourceProvider.java

--- src/main/java/acore/aurora/utility/render/providers/ResourceProvider.java
+++ src/main/java/acore/aurora/utility/render/providers/ResourceProvider.java
@@ -12,6 +12,8 @@ public class ResourceProvider {
     public static final RegistryKey<ShaderProgram> RECTANGLE_BORDER_SHADER_KEY = RegistryKey.of(RegistryKeys.SHADER_PROGRAM, Identifier.ofVanilla("position_color"));
     public static final RegistryKey<ShaderProgram> GLASS_SHADER_KEY = RegistryKey.of(RegistryKeys.SHADER_PROGRAM, Identifier.ofVanilla("position_tex_color"));
 
+    public static final Identifier color_image = Identifier.of("acoreaurora", "textures/gui/colorpicker.png");
+
     public static Identifier getShaderIdentifier(String name) {
         return Identifier.of("acoreaurora", "core/shaders/" + name + ".json");
     }
