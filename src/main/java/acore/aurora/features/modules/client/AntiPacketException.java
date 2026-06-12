3f802271c34cab4d3be430fc13dd745ec54dd253
snowmossd-droid <snowmossd@gmail.com>
2026-06-12 16:06:05 +0700

    Delete src/main directory

--- src/main/java/acore/aurora/features/modules/client/AntiPacketException.java
+++
@@ -1,9 +0,0 @@
-package acore.aurora.features.modules.client;
-
-import acore.aurora.features.modules.Module;
-
-public class AntiPacketException extends Module {
-    public AntiPacketException() {
-        super("NoPacketException", Category.CLIENT);
-    }
-}
