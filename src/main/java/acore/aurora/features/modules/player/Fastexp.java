1629829bd514baccb1688f202a2f98ab31a578c7
snowmossd-droid <snowmossd@gmail.com>
2026-06-15 06:36:38 +0700

    Create Fastexp.java

---
+++ src/main/java/acore/aurora/features/modules/player/Fastexp.java
@@ -0,0 +1,21 @@
+package acore.aurora.features.modules.player;
+
+import acore.aurora.features.modules.Module;
+import acore.aurora.injection.accesors.IMinecraftClient;
+import acore.aurora.setting.Setting;
+import net.minecraft.item.Items;
+
+public class FastExp extends Module {
+    public FastExp() {
+        super("Fastexp", Category.PLAYER);
+    }
+
+    private final Setting<Integer> delay = new Setting<>("Delay", 0, 0, 20);
+
+    @Override
+    public void onUpdate() {
+        if (mc.player == null) return;
+        if (mc.player.getMainHandStack().getItem() != Items.EXPERIENCE_BOTTLE) return;
+        ((IMinecraftClient) mc).setUseCooldown(delay.getValue());
+    }
+}
