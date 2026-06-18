1388d3c9ad258d149ecf4ac337e5d40ea0140477
snowmossd-droid <snowmossd@gmail.com>
2026-06-18 17:06:52 +0700

    Update MaceSwap.java

--- src/main/java/acore/aurora/features/modules/combat/MaceSwap.java
+++ src/main/java/acore/aurora/features/modules/combat/MaceSwap.java
@@ -3,6 +3,7 @@ package acore.aurora.features.modules.combat;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.item.MaceItem;
 import net.minecraft.item.SwordItem;
+import net.minecraft.network.packet.c2s.play.PlayerInteractEntityC2SPacket;
 import net.minecraft.network.packet.c2s.play.UpdateSelectedSlotC2SPacket;
 import net.minecraft.util.Hand;
 import acore.aurora.core.manager.client.ModuleManager;
@@ -15,16 +16,18 @@ import static acore.aurora.features.modules.Module.mc;
 
 public class MaceSwap extends Module {
 
-    public final Setting<Integer> toMaceDelay    = new Setting<>("ToMaceDelay",    50,  0, 500);
+    public final Setting<Integer> toMaceDelay   = new Setting<>("ToMaceDelay",   50,  0, 500);
+    public final Setting<Integer> attackDelay   = new Setting<>("AttackDelay",   120, 0, 500);
     public final Setting<Boolean> attackWithMace = new Setting<>("AttackWithMace", true);
-    public final Setting<Boolean> onlyFalling    = new Setting<>("OnlyFalling",    false);
-    public final Setting<Float>   minFall        = new Setting<>("MinFall",        3f, 0f, 20f, v -> onlyFalling.getValue());
+    public final Setting<Boolean> onlyFalling   = new Setting<>("OnlyFalling",   false);
+    public final Setting<Float>   minFall       = new Setting<>("MinFall",        3f, 0f, 20f, v -> onlyFalling.getValue());
 
-    private final Timer toMaceTimer = new Timer();
+    private final Timer toMaceTimer  = new Timer();
+    private final Timer attackTimer  = new Timer();
 
-    private int savedSlot = -1;
-    private boolean swapped = false;
-    private boolean pendingAttack = false;
+    private int savedSlot         = -1;
+    private boolean swapped       = false;
+    private boolean waitingAttack = false;
 
     public MaceSwap() {
         super("MaceSwap", Category.COMBAT);
@@ -34,9 +37,9 @@ public class MaceSwap extends Module {
     public void onTick(EventTick e) {
         if (mc.player == null || mc.world == null) return;
 
-        boolean hasTarget = ModuleManager.aura.getTarget() != null;
+        var target = ModuleManager.aura.getTarget();
 
-        if (!hasTarget) {
+        if (target == null) {
             restoreIfNeeded();
             return;
         }
@@ -51,7 +54,7 @@ public class MaceSwap extends Module {
             }
         }
 
-        if (!swapped) {
+        if (!swapped && !waitingAttack) {
             if (!toMaceTimer.passedMs(toMaceDelay.getValue())) return;
 
             int maceSlot = findHotbar(true);
@@ -60,17 +63,27 @@ public class MaceSwap extends Module {
             savedSlot = mc.player.getInventory().selectedSlot;
             silentSwap(maceSlot);
             swapped = true;
-            pendingAttack = attackWithMace.getValue();
+
+            if (attackWithMace.getValue()) {
+                waitingAttack = true;
+                attackTimer.reset();
+            }
             return;
         }
 
-        if (pendingAttack) {
-            mc.interactionManager.attackEntity(mc.player, ModuleManager.aura.getTarget());
-            mc.player.swingHand(Hand.MAIN_HAND);
-            pendingAttack = false;
+        if (waitingAttack && swapped) {
+            if (!attackTimer.passedMs(attackDelay.getValue())) return;
 
+            if (ModuleManager.aura.getTarget() != null) {
+                mc.getNetworkHandler().sendPacket(
+                    PlayerInteractEntityC2SPacket.attack(target, mc.player.isSneaking())
+                );
+                mc.player.swingHand(Hand.MAIN_HAND);
+            }
+
+            waitingAttack = false;
             silentSwap(savedSlot);
-            swapped = false;
+            swapped   = false;
             savedSlot = -1;
             toMaceTimer.reset();
         }
@@ -79,21 +92,23 @@ public class MaceSwap extends Module {
     private void restoreIfNeeded() {
         if (swapped && savedSlot != -1) {
             silentSwap(savedSlot);
-            swapped = false;
-            savedSlot = -1;
+            swapped       = false;
+            savedSlot     = -1;
+            waitingAttack = false;
             toMaceTimer.reset();
         }
     }
 
     private void silentSwap(int slot) {
         if (slot < 0 || slot > 8 || mc.player == null) return;
+        mc.player.getInventory().selectedSlot = slot;
         mc.getNetworkHandler().sendPacket(new UpdateSelectedSlotC2SPacket(slot));
     }
 
     private int findHotbar(boolean mace) {
         for (int i = 0; i < 9; i++) {
             var item = mc.player.getInventory().getStack(i).getItem();
-            if (mace && item instanceof MaceItem) return i;
+            if (mace  && item instanceof MaceItem)  return i;
             if (!mace && item instanceof SwordItem) return i;
         }
         return -1;
@@ -104,8 +119,8 @@ public class MaceSwap extends Module {
         if (swapped && savedSlot != -1 && mc.player != null) {
             silentSwap(savedSlot);
         }
-        swapped = false;
-        savedSlot = -1;
-        pendingAttack = false;
+        swapped       = false;
+        savedSlot     = -1;
+        waitingAttack = false;
     }
     }
