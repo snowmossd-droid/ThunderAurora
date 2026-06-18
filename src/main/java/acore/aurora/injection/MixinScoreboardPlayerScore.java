393a471cf0e80048210f1ecc2566b1c079ac8a4e
snowmossd-droid <snowmossd@gmail.com>
2026-06-18 07:53:59 +0700

    Delete src/main directory

--- src/main/java/acore/aurora/injection/MixinScoreboardPlayerScore.java
+++
@@ -1,9 +0,0 @@
-package acore.aurora.injection;
-
-import net.minecraft.scoreboard.ScoreboardEntry;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(ScoreboardEntry.class)
-public class MixinScoreboardPlayerScore {
-
-}
