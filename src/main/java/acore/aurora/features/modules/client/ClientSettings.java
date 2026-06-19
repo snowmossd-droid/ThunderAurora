5728a478ecda9624f96ec9497bc0c9469431188f
snowmossd-droid <snowmossd@gmail.com>
2026-06-19 09:32:18 +0700

    Update ClientSettings.java

--- src/main/java/acore/aurora/features/modules/client/ClientSettings.java
+++ src/main/java/acore/aurora/features/modules/client/ClientSettings.java
@@ -5,7 +5,6 @@ import acore.aurora.setting.Setting;
 
 public final class ClientSettings extends Module {
     public static Setting<Boolean> futureCompatibility = new Setting<>("FutureCompatibility", false);
-    public static Setting<Boolean> customPanorama = new Setting<>("CustomPanorama", true);
     public static Setting<Boolean> customLoadingScreen = new Setting<>("CustomLoadingScreen", true);
     public static Setting<Boolean> scaleFactorFix = new Setting<>("ScaleFactorFix", false);
     public static Setting<Float> scaleFactorFixValue = new Setting<>("ScaleFactorFixValue", 2f, 0f, 4f);
