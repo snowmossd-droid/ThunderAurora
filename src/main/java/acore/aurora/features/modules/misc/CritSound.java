5a42a89f2bc92688003b95bb571562c10e22c5f3
snowmossd-droid <snowmossd@gmail.com>
2026-06-29 17:06:34 +0700

    Update CritSound.java

--- src/main/java/acore/aurora/features/modules/misc/CritSound.java
+++ src/main/java/acore/aurora/features/modules/misc/CritSound.java
@@ -8,33 +8,11 @@ import acore.aurora.events.impl.EventAttack;
 import acore.aurora.features.modules.Module;
 import acore.aurora.setting.Setting;
 
-import java.io.File;
-import java.io.InputStream;
-import java.nio.file.Files;
-
-import static acore.aurora.core.manager.client.ConfigManager.CRITSOUND_FOLDER;
-
 public class CritSound extends Module {
-    public final Setting<Float> volume = new Setting<>("Volume", 1.0f, 0.1f, 2.0f);
     public final Setting<Boolean> onlyCrit = new Setting<>("OnlyCrit", true);
 
     public CritSound() {
         super("CritSound", Category.MISC);
-    }
-
-    @Override
-    public void onEnable() {
-        copyDefaultSound();
-    }
-
-    private void copyDefaultSound() {
-        File dest = new File(CRITSOUND_FOLDER, "critsound.ogg");
-        if (dest.exists()) return;
-        try (InputStream in = getClass().getResourceAsStream("/assets/acoreaurora/sounds/wither.ogg")) {
-            if (in != null) {
-                Files.copy(in, dest.toPath());
-            }
-        } catch (Exception ignored) {}
     }
 
     @EventHandler
@@ -51,6 +29,6 @@ public class CritSound extends Module {
 
         if (onlyCrit.getValue() && !isCrit) return;
 
-        Managers.SOUND.playCritSoundFile(volume.getValue());
+        Managers.SOUND.playWither(1.0f);
     }
 }
