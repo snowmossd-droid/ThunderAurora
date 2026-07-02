0bb2caf79474db87344cfb157af8068fb3f49fcb
snowmossd-droid <snowmossd@gmail.com>
2026-07-02 15:27:16 +0700

    Update Optimizer.java

--- src/main/java/acore/aurora/features/modules/misc/Optimizer.java
+++ src/main/java/acore/aurora/features/modules/misc/Optimizer.java
@@ -8,9 +8,11 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.decoration.ArmorStandEntity;
 import net.minecraft.entity.passive.BatEntity;
 import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.util.math.ChunkPos;
 import acore.aurora.events.impl.EventTick;
 import acore.aurora.features.modules.Module;
 import acore.aurora.setting.Setting;
+import acore.aurora.setting.impl.BooleanSettingGroup;
 import acore.aurora.utility.Timer;
 
 public class Optimizer extends Module {
@@ -38,11 +40,30 @@ public class Optimizer extends Module {
 
     private final Setting<Boolean> aggressiveGC   = new Setting<>("AggressiveGC",   false);
 
+    private final Setting<BooleanSettingGroup> foliageGroup = new Setting<>("OptimizeFoliage", new BooleanSettingGroup(true));
+    private final Setting<Integer> foliageBlend  = new Setting<>("LeafBlend", 0, 0, 7).addToGroup(foliageGroup);
+
+    private final Setting<BooleanSettingGroup> waterGroup   = new Setting<>("OptimizeWater", new BooleanSettingGroup(true));
+
+    private final Setting<BooleanSettingGroup> snowGroup    = new Setting<>("OptimizeSnow", new BooleanSettingGroup(true));
+
+    private final Setting<BooleanSettingGroup> chunkGroup   = new Setting<>("OptimizeChunkLoad", new BooleanSettingGroup(true));
+    private final Setting<Integer> chunkLoadDist  = new Setting<>("LoadDistance", 3, 2, 8).addToGroup(chunkGroup);
+    private final Setting<Integer> chunkSettleTicks = new Setting<>("SettleTicks", 20, 5, 100).addToGroup(chunkGroup);
+
+    private final Setting<BooleanSettingGroup> combatGroup  = new Setting<>("CombatBoost", new BooleanSettingGroup(true));
+    private final Setting<Integer> combatRadius   = new Setting<>("CombatRadius", 12, 4, 32).addToGroup(combatGroup);
+    private final Setting<Integer> combatViewDist = new Setting<>("CombatViewDist", 3, 2, 8).addToGroup(combatGroup);
+    private final Setting<Boolean> combatHideMobs = new Setting<>("HideMobsInCombat", true).addToGroup(combatGroup);
+
     private final Timer gcTimer     = new Timer();
     private final Timer aggrGCTimer = new Timer();
 
     private int savedRenderDist  = -1;
     private boolean savedBobbing = true;
+
+    private ChunkPos lastChunkPos = null;
+    private int ticksSinceChunkChange = 0;
 
     public Optimizer() {
         super("Optimizer", Category.MISC);
@@ -53,6 +74,8 @@ public class Optimizer extends Module {
         if (fullNullCheck()) return;
         savedRenderDist = mc.options.getViewDistance().getValue();
         savedBobbing    = mc.options.getBobView().getValue();
+        lastChunkPos = null;
+        ticksSinceChunkChange = 0;
     }
 
     @Override
@@ -66,6 +89,7 @@ public class Optimizer extends Module {
             mc.options.getCloudRenderMode().setValue(CloudRenderMode.FANCY);
             mc.options.getEntityShadows().setValue(true);
             mc.options.getEnableVsync().setValue(true);
+            mc.options.getBiomeBlendRadius().setValue(2);
         }
         if (mc.world != null) {
             for (Entity en : mc.world.getEntities()) en.setInvisible(false);
@@ -92,6 +116,19 @@ public class Optimizer extends Module {
             mc.options.getBiomeBlendRadius().setValue(0);
         }
 
+        if (foliageGroup.getValue().isEnabled()) {
+            mc.options.getGraphicsMode().setValue(GraphicsMode.FAST);
+            mc.options.getBiomeBlendRadius().setValue(foliageBlend.getValue());
+        }
+
+        if (waterGroup.getValue().isEnabled()) {
+            mc.options.getGraphicsMode().setValue(GraphicsMode.FAST);
+        }
+
+        if (snowGroup.getValue().isEnabled()) {
+            mc.options.getParticles().setValue(ParticlesMode.MINIMAL);
+        }
+
         if (noParticles.getValue()) {
             mc.options.getParticles().setValue(ParticlesMode.MINIMAL);
         }
@@ -110,9 +147,37 @@ public class Optimizer extends Module {
             mc.options.getMaxFps().setValue(fpsLimit.getValue());
         }
 
-        if (reduceDist.getValue()) {
-            mc.options.getViewDistance().setValue(renderDist.getValue());
+        int baseDist = reduceDist.getValue() ? renderDist.getValue() : savedRenderDist;
+        if (baseDist == -1) baseDist = mc.options.getViewDistance().getValue();
+        int targetDist = baseDist;
+
+        if (chunkGroup.getValue().isEnabled() && mc.player != null) {
+            ChunkPos current = new ChunkPos(mc.player.getBlockPos());
+            if (lastChunkPos == null || !current.equals(lastChunkPos)) {
+                lastChunkPos = current;
+                ticksSinceChunkChange = 0;
+            } else if (ticksSinceChunkChange < chunkSettleTicks.getValue()) {
+                ticksSinceChunkChange++;
+            }
+            if (ticksSinceChunkChange < chunkSettleTicks.getValue()) {
+                targetDist = Math.min(targetDist, chunkLoadDist.getValue());
+            }
         }
+
+        boolean inCombat = false;
+        if (combatGroup.getValue().isEnabled() && mc.world != null && mc.player != null) {
+            double radiusSq = (double) combatRadius.getValue() * combatRadius.getValue();
+            for (Entity entity : mc.world.getEntities()) {
+                if (entity == mc.player || !entity.isPlayer()) continue;
+                if (mc.player.squaredDistanceTo(entity) <= radiusSq) {
+                    inCombat = true;
+                    break;
+                }
+            }
+            if (inCombat) targetDist = Math.min(targetDist, combatViewDist.getValue());
+        }
+
+        mc.options.getViewDistance().setValue(targetDist);
 
         if (entityCulling.getValue() && mc.world != null && mc.player != null) {
             int distSq = entityDist.getValue() * entityDist.getValue();
@@ -125,6 +190,7 @@ public class Optimizer extends Module {
                 if (hideAnimals.getValue() && entity instanceof AnimalEntity) hide = true;
                 if (hideArmorStand.getValue() && entity instanceof ArmorStandEntity) hide = true;
                 if (hideAmbient.getValue() && entity instanceof BatEntity) hide = true;
+                if (inCombat && combatHideMobs.getValue() && !entity.isPlayer()) hide = true;
 
                 entity.setInvisible(hide);
             }
